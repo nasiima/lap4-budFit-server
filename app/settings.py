4b4127e27938a0284ad73a09c2b27868c2abe778
nasiima <nasiima.mohamed1@gmail.com>
2022-07-18 18:37:04 +0100

    trying to get date to work

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,8 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
 
 # SQLALCHEMY_DATABASE_URI = 'postgresql://qmcuqdtwphecds:0bd4056db7ad5dda50da5e6d5ce64e7efdf08384b9445dbfb9381f9c50334aa8@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d28ps4h9no69kj'
+
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY ='1eedds23rrr4rrrfrw'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
