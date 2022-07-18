f88e22bc024b4164bd3fba3d56078cb0d4a876e5
nasiima <nasiima.mohamed1@gmail.com>
2022-07-18 20:26:02 +0100

    time now populates db correctly

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -59,8 +59,8 @@ def register():
             req = request.get_json()
             username = req['username']
             password = req['password']
-            # email = req['email']
-            # name = req['name']
+            email = req['email']
+            name = req['name']
             
             user = Users.query.filter_by(username=username).first()
             if user!=None:
