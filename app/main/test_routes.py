b45c2a1456152017d28f6bc1bec748627330f401
n451m <nasim.a.bilal@gmail.com>
2022-07-21 14:41:05 +0100

    18 tests passing 2 failing. delete tests not functioning

--- app/main/test_routes.py
+++ app/main/test_routes.py
@@ -1,4 +1,5 @@
 
+import json
 
 def test_home(api):
     """connects to root route"""
@@ -24,42 +25,54 @@ def test_get_allMatches(api):
     assert app.status == '200 OK'
     assert b'event_id' in app.get_data()
 
+def test_get_one_match(api):
+    app = api.get('/matches/3/')
+    assert app.status == '200 OK'
+    assert b'event_id' in app.get_data()
+
 
 def test_get_oneuser(api):
-    app = api.get('/users/1')
+    app = api.get('/users/1/')
     assert app.status == '200 OK'
     assert b'user_id' in app.get_data()
 
 
-def test_get_event(api):
-    app = api.get('/events/1')
-    assert app.status == '200 ok'
+def test_get_oneevent(api):
+    app = api.get('/events/2/')
+    assert app.status == '200 OK'
     assert b'event_id' in app.get_data()
 
 def test_delete_event(api):
-     app = api.delete('/events/1')
+     app = api.delete('/events/2/')
      assert app.status == '204 NO CONTENT'
      assert b'event_id' in app.get_data()
 
-def test_update_event(api):
-     app = api.patch('/events/1')
-     assert app.status == '201 created'
-     assert b'event_id' in app.get_data()
+# def test_update_event(api):
+#      app = api.patch('/events/1/')
+#      assert app.status == '201 created'
+#      assert b'event_id' in app.get_data()
 
 
 def test_get_user_by_username(api):
-    app = api.get('/users/1')
+    app = api.get('/users/1/')
     assert app.status == '200 OK'
     assert b'user_id' in app.get_data()
 
 
-
-
-   
 
 # def test_delete_cat(self, api):
 #         res = api.delete('/api/cats/1')
 #         assert res.status == '204 NO CONTENT'
+
+def test_handleUserById(api):
+    # res = api.get("/users/1/")
+    # assert res.status == '200 OK'
+    assert app.json[0]['id'] == 1
+    app = app.delete("/events/4/")
+    assert app.status == '204 NO CONTENT'
+
+
+
 
 
 def test_404(api):
